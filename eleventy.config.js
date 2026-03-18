e79410b1b44b8bc39dacbef249dd1b2413fced2d
Seascape SEO Agent <seo@seascape-vacations.com>
2026-03-18 12:04:30 -0400

    Reland duplicate guide retirement and canonical cleanup

--- eleventy.config.js
+++ eleventy.config.js
@@ -31,7 +31,9 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy({ "src/_redirects": "_redirects" });
   eleventyConfig.addPassthroughCopy({ "src/llms.txt": "llms.txt" });
   eleventyConfig.addPassthroughCopy({ "src/robots.txt": "robots.txt" });
-  eleventyConfig.addPassthroughCopy({ "src/guides": "guides" });
+  eleventyConfig.ignores.add("src/guides/anna-maria-island-vacation-cost-guide-2026/**");
+  eleventyConfig.ignores.add("src/guides/best-time-to-visit-anna-maria-island/**");
+  eleventyConfig.ignores.add("src/guides/bradenton-vs-sarasota-vacation-rental-comparison/**");
   
   // Watch for changes during development
   eleventyConfig.addWatchTarget("./_data/");
