ebb2021daf2ec5ce45d334e631e0a53962dea5ef
Seascape SEO Agent <seo@seascape-vacations.com>
2026-06-18 17:21:04 -0400

    Remove obsolete eval fixtures

--- scripts/evals/__fixtures__/_test-missing-lane.js
+++
@@ -1,6 +0,0 @@
-
-const { runLane } = require("/sessions/gracious-peaceful-meitner/mnt/seascape-vacations-site/scripts/evals/lib/run-lane.js");
-runLane(undefined, { apiKey: "", require: false }).then(r => {
-  if (r.skipped && r.ok) process.exit(0);
-  process.exit(1);
-}).catch(() => process.exit(2));
